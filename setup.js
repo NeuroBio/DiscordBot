fedf14629cb0a8887c144680c6af1f7c55bb0668
kArA <kArARedwing@gmail.com>
2024-06-08 12:44:36 -0400

    add post install script

--- setup.js
+++ setup.js
@@ -1,1 +1,44 @@
-console.log('done!');
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filepath = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filepath);
+
+createMainConfig();
+createDevConfig();
+
+
+
+function createMainConfig() {
+	const filePath = path.join(__dirname, 'config.json');
+	const template = {
+		token: "Ask me for this!",
+		clientId: "Ask me for this!",
+		servers: {
+			dev: {
+				name: 'Snake Bucket',
+				id: '1210341550568251393'
+			},
+			public: {
+				name: 'Clone',
+				id: '1210341550568251393'
+			}
+		}
+	};
+	loadOrCreate({ filePath, template});
+}
+
+function createDevConfig () {
+	const filePath = path.join(__dirname, 'dev-config.json');
+	const template = { devServer: 'dev' };
+	loadOrCreate({ filePath, template});
+}
+
+function loadOrCreate({ filePath, template}) {
+	try {
+		fs.readFileSync(filePath);
+	} catch (ignore) {
+		fs.writeFileSync(filePath, JSON.stringify(template, null, 2));
+	}
+}
