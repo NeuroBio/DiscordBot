3179e066d5367b391611060eb0e506c4979f0aa9
kArA <kArARedwing@gmail.com>
2024-06-15 07:01:17 -0400

    add responses

--- src/commands/library/dice/data/eightball.js
+++ src/commands/library/dice/data/eightball.js
@@ -1,5 +1,21 @@
 const EightBallResponses = Object.freeze([
 	'Yes',
+	'No',
+	'Maybe',
+	'Sure',
+	'Unlikely...',
+	'Ask me later...',
+	'I\'d rather not answer that',
+	'*In your dreams.*',
+	'*snort* Yeah right.',
+	'Perhaps if you want it bad enough...',
+	'Depends: are you willing to make a blood sacrifice for it?',
+	'I\'m hearing that\'s a "No".',
+	'Of course!',
+	'I have some bad news...',
+	'AFFIRMATIVE',
+	'NEGATIVE',
+	'DENIED',
 ]);
 
 export default EightBallResponses;
