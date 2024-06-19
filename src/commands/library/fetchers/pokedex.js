bd4be9f8aabcac88269bdf20eaa0cd98367748ae
kArA <kArARedwing@gmail.com>
2024-06-19 06:04:00 -0400

    put together regex for pokedex

--- src/commands/library/fetchers/pokedex.js
+++ src/commands/library/fetchers/pokedex.js
@@ -13,31 +13,62 @@ export default class PokedexCommand extends Command {
 			.setDescription('Returns pokemon data.');
 
 		async function execute (interaction) {
+			const pokedex = await _loadPokedex();
+			console.log(pokedex[0]);
+			interaction.reply(pokedex.length);
+		}
+
+		async function _loadPokedex () {
 			const { data } = await _axios.get('https://www.serebii.net/pokemon/nationalpokedex.shtml', { responseType: 'document' });
 			const serebii = _cheerio.load(data);
 			const table = serebii('main table tbody');
+
 			const pokedex = [];
+			let header = [];
 			table.find('tr').each((i, row) => {
-				const rowData = {};
+				const isHeaderRow = i === 0 || i === 1;
+				if (isHeaderRow) {
+					header = header.concat(_extractHeaderNames({ serebii, row }));
+					return true;
+				}
 
-				serebii(row).find('td').each((j, cell) => {
-					const rawText = serebii(cell).text();
-					const text = rawText.replace(/(\n|\t)/g, '');
-					if (!text || text === ' ') {
-						return true;
-					}
-					rowData[j] = text;
-				});
+				const rowData = _extractRowData({ serebii, row, header });
 
-				if (!rowData[0]) {
+				if (Object.keys(rowData).length === 0) {
 					return true;
 				}
 
 				pokedex.push(rowData);
 			});
-			console.log(pokedex);
-			interaction.reply(pokedex.length);
+
+			return pokedex;
 		}
+
+		function _extractHeaderNames ({ serebii, row }) {
+			const header = [];
+			serebii(row).find('td').each((j, cell) => {
+				const rawText = serebii(cell).text();
+				const text = rawText.replace(/(\n|\t)/g, '');
+				header.push(text);
+			});
+
+			return header;
+		}
+
+		function _extractRowData ({ serebii, row, header }) {
+			const rowData = {};
+			serebii(row).find('td.fooinfo').each((j, cell) => {
+				const rawText = serebii(cell).text();
+				const text = rawText.replace(/(\n|\t)/g, '');
+
+				if (!text || text === ' ') {
+					return true;
+				}
+				rowData[header[j]] = text;
+			});
+			return rowData;
+		}
+
 		super({ data, execute });
 	}
 }
