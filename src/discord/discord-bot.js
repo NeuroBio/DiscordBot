0dcafd0668caebe6ed47fa6b18794cda9fb641a9
kArA <kArARedwing@gmail.com>
2024-06-22 08:25:52 -0400

    finish refactor

--- src/discord/discord-bot.js
+++ src/discord/discord-bot.js
@@ -3,9 +3,7 @@ import CommandLibrary from '../commands/commandLibrary.js';
 import Configs from '../configs.js';
 
 export default class DiscordBot {
-	#Error = Object.freeze({
-		EXECUTE_FAILED: 'There was an error while executing this command.',
-	});
+	#ErrorMessages = Object.freeze({ EXECUTE_FAILED: 'There was an error while executing this command.' });
 
 	#Events;
 	#commandLibrary;
@@ -28,45 +26,40 @@ export default class DiscordBot {
 	}
 
 	async start () {
-
 		const commands = await this.#commandLibrary.load({ excludedFolders: ['no-deploy'] });
+		commands.forEach((command) => this.#client.commands.set(command.data.name, command));
 
-		commands.forEach((command) => {
-			this.#client.commands.set(command.data.name, command);
-		});
-
-		this.#client.once(this.#Events.ClientReady, (readyClient) => {
-			console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-		});
-
-
-		this.#client.on(this.#Events.InteractionCreate, async interaction => {
-			if (!interaction.isChatInputCommand()) {
-				return;
-			}
-			console.log(interaction);
-
-			const command = interaction.client.commands.get(interaction.commandName);
-
-			if (!command) {
-				console.error(`No command matches ${interaction.commandName}.`);
-				return;
-			}
-
-			try {
-				await command.execute(interaction);
-			} catch (error) {
-				console.error(error);
-				if (interaction.replied || interaction.deferred) {
-					await interaction.followUp({ content: this.#Error.EXECUTE_FAILED, ephemeral: true });
-				} else {
-					await interaction.reply({ content: this.#Error.EXECUTE_FAILED, ephemeral: true });
-				}
-			}
-		});
-
-
+		this.#client.once(this.#Events.ClientReady, this.#notifyThatBotIsOnline);
+		this.#client.on(this.#Events.InteractionCreate, this.#executeCommand.bind(this));
 		this.#client.login(this.#Token);
+	}
 
+	async #notifyThatBotIsOnline (readyClient) {
+		console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	}
+
+	async #executeCommand (interaction) {
+		if (!interaction.isChatInputCommand()) {
+			return;
+		}
+		console.log(interaction);
+
+		const command = interaction.client.commands.get(interaction.commandName);
+
+		if (!command) {
+			console.error(`No command matches ${interaction.commandName}.`);
+			return;
+		}
+
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(error);
+			if (interaction.replied || interaction.deferred) {
+				await interaction.followUp({ content: this.#ErrorMessages.EXECUTE_FAILED, ephemeral: true });
+			} else {
+				await interaction.reply({ content: this.#ErrorMessages.EXECUTE_FAILED, ephemeral: true });
+			}
+		}
 	}
 }
